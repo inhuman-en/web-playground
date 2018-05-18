68f7db45d16e78e8924bcafe5e11c55897ee6649
Eugene <inhuman.en@gmail.com>
2018-05-18 15:10:56 +0300

    chore: added material, core and shared modules

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+    imports: [CommonModule, BrowserModule, BrowserAnimationsModule],
+    declarations: [],
+})
+export class CoreModule {}
