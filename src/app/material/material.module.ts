296358f559c375a9e9e24369e0876aaf8491a730
Eugene <inhuman.en@gmail.com>
2018-05-20 19:09:30 +0300

    feat: initial theme

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import { MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule, MatInputModule, MatFormFieldModule } from '@angular/material';
+
+const COMPONENT_MODULES = [MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule, MatInputModule, MatFormFieldModule];
 
 @NgModule({
-    imports: [MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule],
-    exports: [MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule]
+    imports: [...COMPONENT_MODULES],
+    exports: [...COMPONENT_MODULES]
 })
 export class MaterialModule {}
