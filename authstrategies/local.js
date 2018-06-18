b80e841b09d2b912f78fd7a8517395d6136625ab
Eugene <inhuman.en@gmail.com>
2018-06-18 20:12:03 +0300

    feat: no-password auth

--- authstrategies/local.js
+++ authstrategies/local.js
@@ -1,12 +1,21 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../db/models/User');
 
 module.exports = new LocalStrategy(function(username, password, done) {
-    if (username === 'admin' && password === '1') {
-        return done(null, {
-            username,
-            password
-        });
-    } else {
-        return done(null, false, { message: 'Incorrect login or password' });
-    }
+    User.findOne({ username }, function(err, user) {
+        if (err) {
+            return done(err);
+        }
+
+        if (!user) {
+            return done(null, false, { message: 'Incorrect login or password' });
+        }
+        // if (!user.validPassword(password)) {
+        //     return done(null, false, { message: 'Incorrect login or password' });
+        // }
+
+        console.log(`authenticating ${username} successful`, user);
+
+        return done(null, user);
+    });
 });
