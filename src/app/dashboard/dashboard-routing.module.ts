16255099e7c5d2b0b8d7a3bcaf1ad831481c42c8
Eugene <inhuman.en@gmail.com>
2018-06-21 11:25:45 +0300

    protecting dashboard route

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardPageComponent } from './dashboardpage/dashboardpage.component';
+import { AuthGuard } from '../auth';
 
-const routes: Routes = [{ path: 'dashboard', component: DashboardPageComponent}];
+const routes: Routes = [
+    {
+        path: 'dashboard',
+        component: DashboardPageComponent,
+        canActivate: [AuthGuard]
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
